031b4b9f0dfb66555784f29338ceba19e6139752
SaFFireGHOST <Koushik.Mupparapu@iiitb.ac.in>
2025-04-18 13:29:59 +0530

    changed , running format

--- main.py
+++ main.py
@@ -585,5 +585,5 @@ def reset_feedback():
 
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True, host="0.0.0.0", port="8080")
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port="8080")
